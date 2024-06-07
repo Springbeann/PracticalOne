4dc7e206630d21fce541916cce956881bf00edaa
Springbeann <kreepietoast@gmail.com>
2024-06-07 11:57:39 +0200

    final version of PracticalGAs

--- PracticalGAs.java
+++ PracticalGAs.java
@@ -1,160 +1,173 @@
-import java.lang.annotation.Target;
 import java.util.Random;
-import java.util.function.DoublePredicate;
 
 /**
- * Some very basic stuff to get you started. It shows basically how each
- * chromosome is built.
- * 
- * @author Jo Stevens
- * @version 1.0, 14 Nov 2008
- * 
- * @author Alard Roebroeck
+ * Basic Genetic Algorithm to evolve a string to match a target phrase.
+ *
  * @version 1.1, 12 Dec 2012
- * 
  */
-
 public class PracticalGAs {
 
     static final String TARGET = "HELLO WORLD";
     static char[] alphabet = new char[27];
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         int popSize = 100;
+        int generations = 0;
+        boolean found = false;
+        initializeAlphabet();
+
+        Random generator = new Random(System.currentTimeMillis());
+        Individual[] population = new Individual[popSize];
+
+        // Initialize the population with random characters
+        for (int i = 0; i < popSize; i++) {
+            char[] tempChromosome = new char[TARGET.length()];
+            for (int j = 0; j < TARGET.length(); j++) {
+                tempChromosome[j] = alphabet[generator.nextInt(alphabet.length)];
+            }
+            population[i] = new Individual(tempChromosome);
+        }
+
+        while (!found && generations < 10000) { // Limiting to 10000 generations
+            generations++;
+            double[] fitnessScores = calculateFitness(population, TARGET);
+
+            // Check if target is found
+            for (int i = 0; i < population.length; i++) {
+                if (fitnessScores[i] == 1.0) {
+                    System.out.println("Found target at generation " + generations + ": " + population[i].genoToPhenotype());
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) break;
+
+            Individual[] newPopulation = new Individual[popSize];
+
+            // Create new population using roulette wheel selection and crossover
+            for (int i = 0; i < popSize; i++) {
+                Individual parent1 = roulette(population, fitnessScores);
+                Individual parent2 = roulette(population, fitnessScores);
+                newPopulation[i] = crossover(parent1, parent2);
+                mutate(newPopulation[i], 0.01); // Mutation rate is 1%
+            }
+
+            population = newPopulation;
+        }
+
+        if (!found) {
+            System.out.println("Target not found within 10000 generations.");
+        } else {
+            System.out.println("Generations taken: " + generations);
+        }
+    }
+
+    private static void initializeAlphabet() {
         for (char c = 'A'; c <= 'Z'; c++) {
             alphabet[c - 'A'] = c;
         }
         alphabet[26] = ' ';
-        Random generator = new Random(System.currentTimeMillis());
-        Individual[] population = new Individual[popSize];
-        // we initialize the population with random characters
-        for (int i = 0; i < popSize; i++) {
-            char[] tempChromosome = new char[TARGET.length()];
-            for (int j = 0; j < TARGET.length(); j++) {
-                tempChromosome[j] = alphabet[generator.nextInt(alphabet.length)]; //choose a random letter in the alphabet
-            }
-            population[i] = new Individual(tempChromosome);
-        }
-        // What does your population look like?
-        for (int i = 0; i < population.length; i++) {
-            System.out.println(population[i].genoToPhenotype());
-        }
-        
-        // do your own cool GA here
-        double[] fitnessScores; 
-        Individual[] parents; 
-        for(int i = 0; i < population.length; i++){
-            fitnessScores = calculateFitness(population, TARGET);
-            for(int j = 0; j < fitnessScores.length; j++){
-                System.out.print(fitnessScores[j] + " ");
-            }
-            System.out.println();
-
-            parents = selectParents(population, fitnessScores); 
-            for(int l = 0; l< parents.length; l++){
-                char[] chromosome = parents[l].getChromosome();
-                for(int k = 0; k < chromosome.length; k++){
-                    System.out.print(chromosome[k] + " "); 
-                }
-                System.out.println(); 
-            }
-            System.out.println();
-        }
     }
 
-    public static double[] calculateFitness(Individual[] pop, String target){
-        double fitness = 0; 
-        double[] scores = new double[pop.length]; 
-        char[] ch = new char[target.length()]; 
-        double totalScore = 0; 
-        for(int i = 0; i<target.length(); i++){
-            ch[i] = target.charAt(i); 
-        }
-        
-        for(int i = 0; i<pop.length; i++){
-            fitness = 0; 
-            char[] current = pop[i].getChromosome(); 
-            for(int j = 0; j<current.length; j++){
-                if(current[j] == ch[j]){
-                    fitness++; 
+    private static double[] calculateFitness(Individual[] pop, String target) {
+        double[] scores = new double[pop.length];
+        char[] targetChars = target.toCharArray();
+
+        for (int i = 0; i < pop.length; i++) {
+            double fitness = 0;
+            char[] current = pop[i].getChromosome();
+
+            for (int j = 0; j < current.length; j++) {
+                if (current[j] == targetChars[j]) {
+                    fitness++;
                 }
             }
-            scores[i] = fitness; 
-            double individualScore = fitness / target.length(); 
-            scores[i] = individualScore; 
-            pop[i].setFitness(individualScore); 
+
+            double individualScore = fitness / target.length();
+            scores[i] = individualScore;
+            pop[i].setFitness(individualScore);
         }
-        return scores; 
+        return scores;
     }
 
-    public static Individual[] selectParents(Individual[] pop, double[] scores){
-        Random rand = new Random(); 
-        HeapSort.sort(pop); 
-        double totalFitness = 0; 
-        double randDouble = 0;  
-        Individual parentOne = null; 
-        Individual parentTwo = null; 
-        Individual[] parents = new Individual[2]; 
+    private static Individual roulette(Individual[] pop, double[] scores) {
+        double totalFitness = 0;
 
-        for(int i = 0; i<pop.length; i++){
-            totalFitness += scores[i]; 
+        for (double score : scores) {
+            totalFitness += score;
         }
-        for(int i = 0; i<pop.length; i++){
-            scores[i] = scores[i] / totalFitness; 
+
+        double[] cumulativeProbabilities = new double[scores.length];
+        cumulativeProbabilities[0] = scores[0] / totalFitness;
+
+        for (int i = 1; i < scores.length; i++) {
+            cumulativeProbabilities[i] = cumulativeProbabilities[i - 1] + (scores[i] / totalFitness);
         }
-        
-        while(parentOne == null || parentTwo == null){
-            int parentIndx = 0; 
-            double tempSum = 0; 
-            randDouble = rand.nextDouble(); 
-            for(int i = 0; i<pop.length; i++){
-                tempSum += scores[i]; 
-                if(tempSum >= randDouble){
-                    parentIndx = i;
-                    break; 
-                }
-            }
-            if(parentOne == null){
-                parentOne = pop[parentIndx]; 
-            } else if(parentTwo == null && parentOne != pop[parentIndx]){
-                parentTwo = pop[parentIndx]; 
+
+        Random rand = new Random();
+        double randDouble = rand.nextDouble();
+
+        for (int i = 0; i < cumulativeProbabilities.length; i++) {
+            if (randDouble <= cumulativeProbabilities[i]) {
+                return pop[i];
             }
         }
-        parents[0] = parentOne; 
-        parents[1] = parentTwo; 
-
-        return parents; 
+        return pop[pop.length - 1]; // Return the last individual in case of rounding issues
     }
 
-    public static double Mutation(){
-        Random rand = new Random(); 
-        return rand.nextDouble(); 
+    private static Individual crossover(Individual parent1, Individual parent2) {
+        Random random = new Random();
+        char[] chromosome1 = parent1.getChromosome();
+        char[] chromosome2 = parent2.getChromosome();
+        char[] newChromosome = new char[chromosome1.length];
+        for (int i = 0; i < chromosome1.length; i++) {
+            newChromosome[i] = random.nextBoolean() ? chromosome1[i] : chromosome2[i];
+        }
+        return new Individual(newChromosome);
     }
 
-    public static char mutatedLetter(){
-        Random rand = new Random(); 
-        return (char)(rand.nextInt(26) + 'A'); 
-    }
-
-    public static void offSpring(Individual[] parents, double rndmChance){
-        Random rand = new Random(); 
-        double rndmCh = Mutation(); 
-        
-        Individual[] offspring = new Individual[1]; 
-        for(int i = 0; i<parents.length; i++){
-            double temp = 0; 
-            for(int j = 0; j<parents[i].getChromosome().length; j++){
-                temp = Mutation(); 
-                if(temp < rndmChance){
-                    char newGene =  mutatedLetter(); 
-                    char[] chromosome = parents[i].getChromosome(); 
-                    chromosome[j] = newGene; 
-                    parents[i].setChromosome(chromosome); 
-                }
+    private static void mutate(Individual individual, double mutationRate) {
+        Random random = new Random();
+        char[] chromosome = individual.getChromosome();
+        for (int i = 0; i < chromosome.length; i++) {
+            if (random.nextDouble() < mutationRate) {
+                chromosome[i] = alphabet[random.nextInt(alphabet.length)];
             }
         }
+        individual.setChromosome(chromosome);
+    }
+}
+
+class Individual {
+    private char[] chromosome;
+    private double fitness;
+
+    public Individual(char[] chromosome) {
+        this.chromosome = chromosome;
+    }
+
+    public char[] getChromosome() {
+        return chromosome;
+    }
+
+    public void setChromosome(char[] chromosome) {
+        this.chromosome = chromosome;
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    public String genoToPhenotype() {
+        return new String(chromosome);
+    }
+}
+
+class HeapSort {
+    public static void sort(Individual[] array) {
+        // Implement heap sort for Individual array
+        // Sorting logic goes here
+        //did not use heap sort for this
     }
 }
